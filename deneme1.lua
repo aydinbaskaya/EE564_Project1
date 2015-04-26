a3b55a36d89330a415b7985317b6803cc10e7342
Aydın Başkaya <aydin.baskaya@metu.edu.tr>
2015-04-26 22:10:59 +0300

    femm ve lua dosyası
    2-boyutlu hareket

---
+++ deneme1.lua
@@ -0,0 +1,31 @@
+showconsole()
+mydir="./"
+open(mydir .. "deneme1.fem")
+mi_saveas(mydir .. "demo.fem")
+mi_seteditmode("group")
+vx_init=0
+vy_init=0
+dt=0.05
+m=1.101
+for n=0,15 do
+    mi_analyze()
+    mi_loadsolution()
+    mo_groupselectblock(1)
+    fx=mo_blockintegral(18)
+    fy=mo_blockintegral(19)-(m*9.8)
+    ax=(fx/m)
+    ay=(fy/m)
+    dvx=ax*dt
+    dvy=ay*dt
+dx=vx_init*dt+(0.5*ax*dt*dt)
+dy=vy_init*dt+(0.5*ay*dt*dt)
+dx=dx/0.0254
+dy=dy/0.0254
+vx_init=vx_init+dvx
+vy_init=vy_init+dvy
+print(fx,fy)
+    if (n<15) then
+        mi_selectgroup(1)
+        mi_movetranslate(dx,dy)
+    end
+end
